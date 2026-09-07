3b9be358c9483aa6b2512b308cd8e5a92f0ce353
Sonam <sonamyadav9351@gmail.com>
2026-09-07 10:14:26 +0530

    Time: 17 ms (4.54%), Space: 46.9 MB (5.13%) - LeetHub

---
+++ 0541-reverse-string-ii/0541-reverse-string-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String reverseStr(String s, int k) {
+        String str = "";
+        int i=0;
+        while(i<s.length()){
+            str += reverse(s,i,i+k-1);
+            str += same(s,i+k,i+2*k-1);
+            i=i+2*k;
+        }
+        return str;        
+    }
+    public static  String reverse(String s,int i,int j){
+        String str = "";
+        if(j>s.length()-1) j=s.length()-1;
+        for(int k=j;k>=i;k--){
+            str += s.charAt(k);
+        }
+        return str;
+    }
+    public static String same(String s,int i,int j){
+        String str = "";
+        if(j>s.length()-1) j=s.length()-1;
+        for(int k=i;k<=j;k++){
+            str += s.charAt(k);
+        }
+        return str;
+    }
+}
