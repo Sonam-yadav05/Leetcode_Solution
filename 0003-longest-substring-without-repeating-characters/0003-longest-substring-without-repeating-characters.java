4f510182a2b5e46bd5153cfbca1aeae28550b81f
Sonam <sonamyadav9351@gmail.com>
2026-09-11 10:11:14 +0530

    Time: 21 ms (62.05%), Space: 47.9 MB (42.08%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -0,0 +1,66 @@
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+
+        if (n == 0) {
+            return 0;
+        }
+
+        int i = 0;
+        int j = 0;
+        int maxSize = 0;
+
+        while (j < n) {
+            int k = i;
+            boolean duplicate = false;
+
+            while (k < j) {
+                if (s.charAt(k) == s.charAt(j)) {
+                    i = k + 1;
+                    duplicate = true;
+                    break;
+                }
+                k++;
+            }
+
+            int size = j - i + 1;
+            maxSize = Math.max(size, maxSize);
+
+            j++;
+        }
+
+        return maxSize;
+    }
+}
+
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         int n = s.length();
+//         if (n == 0) return 0; 
+//         int i=0;
+//         int j=1;
+//         int maxSize = 1;
+//         while(j<n){
+//             int k=i;
+//             int size = 1;
+//             while(k<j && j<n){
+//                 if(s.charAt(k)!=s.charAt(j)){
+//                     k++;
+//                     size = j - i + 1;
+//                     maxSize = Math.max(size,maxSize);
+//                 }
+//                 else{
+//                     size = j - i;
+//                     maxSize = Math.max(size,maxSize);
+//                     i=k+1;
+//                     k=i;
+//                     break;
+//                 }
+//             }
+//             j++;
+//         }
+//         return maxSize;
+        
+//     }
+// }
